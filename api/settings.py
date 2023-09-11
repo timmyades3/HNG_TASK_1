4820e9aa275d7dca7bf8d8b2e18ee64e20f44e9a
timmyades3 <oluwatimileyinadesina623@gmail.com>
2023-09-11 20:06:10 +0100

    updated

--- api/settings.py
+++ api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'rest_framework',
     'stackapi.apps.StackapiConfig'
 ]
@@ -129,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = 'staticfiles_build/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
