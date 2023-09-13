1aa646978a7953c36f868f73fffedeb5433cbcd3
timmyades3 <oluwatimileyinadesina623@gmail.com>
2023-09-13 20:55:53 +0100

    updated

--- api/urls.py
+++ api/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 
@@ -21,5 +20,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('stackapi.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
